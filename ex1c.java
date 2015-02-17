85b0ddacbe3d5cacd59c95f2df8db9c8881a442d
Nicolai Skogheim <nicolai.skogheim@gmail.com>
2015-02-17 16:26:32 +0100

    Adds 1b and 1c

---
+++ ex1c.java
@@ -0,0 +1,12 @@
+//         v--- her mangler det noe!
+class ex1c 
+  //           v
+  private staticc int foo = -1;
+
+  //                v
+  public static StriXg bar = "Hei";
+
+  public static boolean baz(Str qux){
+    return false;
+  }
+}

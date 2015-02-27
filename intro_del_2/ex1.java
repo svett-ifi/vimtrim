91236de6b9f9653ae961eef8d5acba9e3d38fc6e
Nicolai Skogheim <nicolai.skogheim@gmail.com>
2015-02-27 20:54:31 +0100

    Adds skeleton for first exercise in lvl2

---
+++ intro_del_2/ex1.java
@@ -0,0 +1,17 @@
+public class ex1 {
+
+  protected static final int CODE = 123;
+
+  public static void main (String[] args) {
+
+    boolean allGood = true;
+    
+    if ( allGood ) {
+      openSafe(CODE);
+    }
+  }
+
+  private static void openSafe(int code) {
+  
+  }
+}

f5b4a89a4fdc66b9610b998627f145ede07e4829
Nicolai Skogheim <nicolai.skogheim@gmail.com>
2015-03-01 22:48:10 +0100

    Adds starting point for 2.1b

---
+++ ex1b.java
@@ -0,0 +1,17 @@
+class ex1b {
+
+    // bruk [ $daw ] for å slette v
+    public int secret = 234; slettmeg
+    public String verySecret = "qwerty123"; megogså
+
+    // [ $. ] kanskje ? punktum skal gjenta [ daw ]
+    public static boolean truth = true; menialledager
+
+    private String the(int cake){
+      if(cake == 43) {
+        return "is a lie";
+      } else {
+        return "nothing to see here. move along."
+      }
+    }
+}

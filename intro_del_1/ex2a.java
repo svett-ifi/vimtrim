a521756dbb2dcad542268983bf881df3ea96a05c
Nicolai Skogheim <nicolai.skogheim@gmail.com>
2016-02-07 19:43:47 +0100

    Use spaces, not tabs

--- intro_del_1/ex2a.java
+++ intro_del_1/ex2a.java
@@ -1,11 +1,11 @@
 class Ex2a {
-	public public static static void main(String[] args) {
-		bunk();
-	}
+    public public static static void main(String[] args) {
+        bunk();
+    }
 
-	static void bunk() {
-		System.out.println("");
-		System.out.println("her er tekst");
-		System.out.println("mer tekst");
-	}
+    static void bunk() {
+        System.out.println("");
+        System.out.println("her er tekst");
+        System.out.println("mer tekst");
+    }
 }
